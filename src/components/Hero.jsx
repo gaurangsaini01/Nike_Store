cbcc296173bee57712275728ba39c66b66e96e1b
gaurangsaini01 <gaurangsaini01@gmail.com>
2024-04-10 14:10:55 +0530

    Addes Some Links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = ({ title, subtitle, btntext, img, sociallinks, videos }) => {
           <div className='grid items-center justify-items-center mt-28 md:mt-24'>
             <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{title}</h1>
             <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{subtitle}</h1>
-            <button type='button' className='button-theme bg-slate-200 hover:scale-105  shadow-slate-200 rounded-xl my-5'>{btntext}</button>
+            <button type='button' className='button-theme bg-slate-200 hover:scale-105  shadow-slate-200 rounded-xl my-5'><a href="https://www.nike.com/si/a/nike-adapt-bb-release-info" target='_blank'>{btntext}</a></button>
             <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
               {videos?.map((val, i) => (
                 <Clips
